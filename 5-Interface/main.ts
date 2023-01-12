2a9fc8cf51088146ea5f4f3247ac2a5d49862669
Abhijeet Shende <GS-3467@GSLAB.COM>
2023-01-12 17:29:38 +0530

    interfaces with class

--- 5-Interface/main.ts
+++ 5-Interface/main.ts
@@ -4,13 +4,23 @@ interface Person {
   sayHello: () => void;
 }
 
-let p2:Person ={
-  name: 'abhijeet',
+let p2: Person = {
+  name: "abhijeet",
   age: 32,
   sayHello: function (): void {
-    console.log('hello world');
-    
+    console.log("hello world");
+  },
+};
+
+p2.sayHello();
+
+class PersonEx implements Person {
+  constructor(public name: string, public age: number) {}
+  sayHello() {
+    console.log("hello from PersonEx");
   }
 }
 
-p2.sayHello();
+
+let p3 = new PersonEx('abhijeet',35)
+p3.sayHello();
--- dist/5-Interface/main.js
+++ dist/5-Interface/main.js
@@ -1,10 +1,21 @@
 "use strict";
 let p2 = {
-    name: 'abhijeet',
+    name: "abhijeet",
     age: 32,
     sayHello: function () {
-        console.log('hello world');
-    }
+        console.log("hello world");
+    },
 };
 p2.sayHello();
+class PersonEx {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    sayHello() {
+        console.log("hello from PersonEx");
+    }
+}
+let p3 = new PersonEx('abhijeet', 35);
+p3.sayHello();
 //# sourceMappingURL=main.js.map
